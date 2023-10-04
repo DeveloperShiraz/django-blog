2a68b1ac225f61bcac7a221887d6d138bf13be13
Mohammed Shiraz <lightningboy78@gmail.com>
2023-10-04 11:31:37 -0400

    Alpha:0.4 - Added Profile Pictures Using Signals.

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) #We add CASCADE here because. If the user is deleted. Their Profile is also deleted.
+    image = models.ImageField(default='default.jpg',upload_to='profile_pictures')
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
